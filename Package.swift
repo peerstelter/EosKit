a213c7ad425f163107555c635b98267cff6ff5d1
peerstelter <peerstelter@gmx.de>
2021-07-21 08:04:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "OSCKIT", url: "https://github.com/SammySmallman/OSCKit", from: "2.1.5"),
+        .package(url: "https://github.com/SammySmallman/OSCKit", from: "2.1.5"),
         .package(name: "NetUtils", url: "https://github.com/svdo/swift-netutils", from: "4.1.0"),
     ],
     targets: [
